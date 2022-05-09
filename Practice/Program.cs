3086abdc97c7927fa4cfcaeebdfc4d9234343c41
anh.le <anh.le@PC-ANHLE>
2022-05-09 16:55:55 +0700

    Add project files.

---
+++ Practice/Program.cs
@@ -0,0 +1,58 @@
+﻿using Autofac;
+
+namespace Practice
+{
+    public interface IWork
+    {
+        void Working();
+    }
+    public class Student : IWork
+    {
+        public void Working()
+        {
+            Console.WriteLine("Student is learing...");
+        }
+    }
+    public class Teacher : IWork
+    {
+        public void Working()
+        {
+            Console.WriteLine("Teacher is teaching...");
+        }
+    }
+    public class Classroom
+    {
+        private IWork _work;
+        public Classroom(IWork work)
+        {
+            this._work = work;
+        }
+        public void Working()
+        {
+            _work.Working();
+        }
+    }
+    
+    public class Program
+    {
+        private static IContainer Container { get; set; }   
+        public static void Main(string[] args)
+        {
+           var builder =new ContainerBuilder();
+
+            builder.RegisterType<Student>().As<IWork>();
+            Container= builder.Build();
+
+            Working();
+
+        }
+        public static void Working()
+        {
+            using (var scope=Container.BeginLifetimeScope())
+            {
+                var work = scope.Resolve<IWork>();
+                work.Working();
+            }
+        }
+    }
+}
